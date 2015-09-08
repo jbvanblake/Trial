ffb95ec0ed2e2fd66f0fb52ddea5e8d30bba2282
jbvanblake <josh@vanblake.com>
2015-09-08 14:53:57 -0700

    refactoring buggy delete to know delete by id reather than by parent.

--- pagesWidget.js
+++ pagesWidget.js
@@ -1,21 +1,34 @@
 define(["jquery",
+		"underscore",
  		"handlebars",
-  		"text!views/mainPageTemplate.html"], 
+  		"text!views/mainPageTemplate.html",
+  		"text!views/existingPageTemplate.html"], 
 
-	function(jquery, handlebars, pageTemplate){
+	function(jquery, _, handlebars, pageTemplate, existingPageTemplate){
+		var pages = [], element;
 
-		var initialize = function(element){
+		var initialize = function(el){
+			element = el;
 			var wholePage = handlebars.compile(pageTemplate);
-			element.append(wholePage());
+			var html = wholePage();
+			element.append(html);
+
+			element.find(".page-create").click(addPage);
+
+		};
+		var addPage = function(event){
+			var template = handlebars.compile(existingPageTemplate);
+			var newPageId = pages.length+1;
+			element.find(".editSectionContainer").append(template({id:newPageId}));
+			pages.push({id:newPageId, el:template({id:newPageId})});
 
 			element.find(".page-delete").click(removePage);
 
+		}
 
-
-
-		};
 		var removePage = function(event){
-			event.target.parentElement.remove();
+			var pageId = $(event.target.parentElement).data("id");
+			element.find("[data-id=" + pageId + "]").remove();
 		}
 
 		return {
--- requireConfig.js
+++ requireConfig.js
@@ -1,6 +1,7 @@
 require.config({
 	paths:{
         jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min",
+        underscore: "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min",
         bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min",
         handlebars: "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.2/handlebars",
         text: "https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min"
