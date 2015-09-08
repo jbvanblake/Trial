d96e46845e3cdbf9999b7e12200db2833a8071c2
jbvanblake <josh@vanblake.com>
2015-09-08 12:28:47 -0700

    decided to add require js.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-	alert("main");
+define(["jquery"], 
+	function(jquery){
+		alert("require");
+
 });
+
+
+// $(document).ready(function(){
+// 	//alert("main");
+// 	// var pageTemplate = $("#pageTemplate").html()
+// 	// var template = Handlebars.compile(pageTemplate);
+
+
+// 	//$("body").append(template());
+
+// });
---
+++ requireConfig.js
@@ -0,0 +1,8 @@
+require.config({
+	paths:{
+        jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min"
+
+	}
+});
+
+require(['main']);
